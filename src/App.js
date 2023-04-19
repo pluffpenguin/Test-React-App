b8634d1179817e6d5fc5ac48ab8b67d387067060
Kiaran Lau <76144488+pluffpenguin@users.noreply.github.com>
2023-04-19 14:55:02 -0700

    new iframe edit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './Components.css'
+// import { Code } from 'mongodb';
+import React, { Component } from 'react';
 
 function MyButton(){
   return (
-    <button>Example Button</button>
+    <button className='MyButton'>Example Button</button>
   );
+}
+
+class CustomComponent extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      message: ''
+    }
+  }
+
+  
 }
 
 function App() {
   return (
-    // <div className="App">
+    <div className='row'>
+        <div className='col-6'>
+          <h1>First test website project.</h1>
+          <p>Paragraph. </p>
+          <div>
+            <img className="avatar" src="jayform.jpg" alt='logo'/>
+            <div>
+              <MyButton/>
+              <iframe title='Stream' classNam e='iFrame' src="https://bestsolaris.com/solaris.php?postid=32387" allowfullscreen allowtransparency></iframe>
+            </div>
+          </div>
+        </div>
+        <div className='col-6'></div>
+    </div>
+  );
+}
+
+export default App;
+
+
+/* <div className='row'>
+        <div className='col-6'>
+          <h1>First test website project.</h1>
+          <p>Paragraph. </p>
+          <div>
+            <img className="avatar" src="jayform.jpg" alt='logo'/>
+            <div>
+            <MyButton/>
+            </div>
+          </div>
+        </div>
+        <div className='col-6'></div>
+    </div> */
+
+// <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
     //     <p>
@@ -23,15 +71,5 @@ function App() {
     //     >
     //       Learn React
     //     </a>
-    //   </header>
+    //   </header>cls
     // </div>
-    <div>
-      <h1>First test website project.</h1>
-      <p>Paragraph. </p>
-      <img className="avatar" src="jayform.jpg"/>
-      <MyButton />
-    </div>
-  );
-}
-
-export default App;
